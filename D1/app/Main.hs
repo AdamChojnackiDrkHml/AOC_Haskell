6aecb809f0d5b453babd0b0ebea671ec5c309f7a
AdamChojnackiDrkHml <adam.chojnacki.syl@gmail.com>
2023-12-11 20:32:53 +0100

    AOC Ex 1 Part 2 done

--- D1/app/Main.hs
+++ D1/app/Main.hs
@@ -1,23 +1,35 @@
 module Main where
 import Data.Char (isDigit, digitToInt)
-import Text.Replace
+import Data.Text (replace, Text, pack, unpack)
 import GHC.IO.FD (openFile)
 import GHC.IO.IOMode (IOMode(ReadMode))
 import GHC.IO.Handle (hClose)
-
+import qualified Data.Text as T
 main :: IO ()
 main = do
-    contents <- readFile "data/data"
+    contents <- readFile "D1/data/data"
     -- putStr . show . simpleTask $ contents
     (print . workOnContents) contents
 
-numbers :: [String]
-numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"]
 
-findAndReplaceLetterDigit line digitString = 
+digitsStrings :: [String]
+digitsStrings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"]
+
+digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+findAndReplaceLetterDigits :: [(Text, Text)] -> Text ->  Text
+findAndReplaceLetterDigits ((x, y):xs) line  = findAndReplaceLetterDigits xs (replace x (T.concat [pack [T.head x], y, pack [T.last x]]) line)
+findAndReplaceLetterDigits [] line = line
+
+normalizeLine :: Text -> Text
+normalizeLine = findAndReplaceLetterDigits $ mapPair pack $ zip digitsStrings digits
+
+takeDigitsFromLine2 :: String -> [Int]
+takeDigitsFromLine2 = firstAndLast. map digitToInt . filter isDigit . unpack . normalizeLine . pack
 
 takeDigitsFromLine :: [Char] -> [Int]
 takeDigitsFromLine = firstAndLast. map digitToInt . filter isDigit
+
 
 firstAndLast :: [a] -> [a]
 firstAndLast xs = head xs:[last xs]
@@ -27,7 +39,7 @@ twoDigitsToNumber (x:y:_) = 10 * x + y
 twoDigitsToNumber _ = undefined
 
 workOnLine :: [Char] -> Int
-workOnLine = twoDigitsToNumber . takeDigitsFromLine
+workOnLine = twoDigitsToNumber . takeDigitsFromLine2
 
 
 workOnContents :: String -> Int
@@ -35,3 +47,7 @@ workOnContents = sum . map workOnLine . lines
 
 simpleTask :: String -> [[Int]]
 simpleTask content = map takeDigitsFromLine $ lines content
+
+mapPair :: (t -> b) -> [(t, t)] -> [(b, b)]
+mapPair f ((a, b):xs) = (f a, f b):mapPair f xs
+mapPair f [] = []
